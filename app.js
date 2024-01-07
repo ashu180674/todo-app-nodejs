f93671806312061be932308073848df309931519
ashu180674 <ashutripathi16.20.2001@gmail.com>
2024-01-07 23:40:49 +0530

    added multiple routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 let items = ["DSA","kubernetes","python"];
+let workitems = [];
+
 
 app.get("/",function(req,res){
 
@@ -19,17 +21,35 @@ app.get("/",function(req,res){
  let day= today.toLocaleDateString("en-US",options);
 
 
-res.render('list',{ kindofday: day,newitem:items });
+res.render('list',{ listtitle: day,newitem:items });
+
 
 });
 
 app.post("/",function(req,res) {
   
 let item = req.body.newitem;
- items.push(item);
+ if(req.body.list=="work"){
+ 
+ workitems.push(item);
+ res.redirect("/work");
+
+ }else{
+    items.push(item);
  res.redirect("/");
+ }
+
+
 })
 
+// let item = req.body.newitem;
+// workitems.push(item);
+// res.redirect("/work");
+
+app.get("/work",function(req,res){
+ res.render('list',{listtitle: "work list",newitem:workitems})
+    
+    })
 
 
 
